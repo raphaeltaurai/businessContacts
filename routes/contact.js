a01e9fa9c2eab4ece456bd3cc28f96364170f6e9
Raphael Shawn Taurai <131987084+raphaeltaurai@users.noreply.github.com>
2025-09-10 10:27:29 +0200

    Update and rename users.js to contact.js

---
+++ routes/contact.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const contactController = require('../controllers/contact');
+const router = express.Router();
+
+//GET /feed /post
+router.get('/', contactController.getData);
+//localhost:3000/contacts
+module.exports = router;
